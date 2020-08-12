5ce18bd61de04dbaa6676e61228a208bb850044b
lawrence <laguilera03@gmail.com>
2020-08-11 20:09:55 -0400

    secret added

--- obj/Release/netcoreapp3.1/api-user-secrets.AssemblyInfo.cs
+++ obj/Release/netcoreapp3.1/api-user-secrets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("walkthrough-secrets")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-user-secrets")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-user-secrets")]
